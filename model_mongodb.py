5d7f8d3778b55f4a48e9c0961d87c875b0243d58
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2023-03-20 10:06:23 -0300

    criação da tabela users em mongo

--- model_mongodb.py
+++ model_mongodb.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager
 
-
 login = LoginManager(app)
 
 
@@ -11,20 +10,21 @@ login = LoginManager(app)
 def load_user(id):
     return User.objects.get(id=id)
 
+
 class User(UserMixin, db.Document):
     meta = {'collection': 'users'}
     name = db.StringField(default=True)
-    sobrenome = db.StringField(default=True)
+    lastname = db.StringField(default=True)
     email = db.EmailField(unique=True)
     password_hash = db.StringField(default=True)
     active = db.BooleanField(default=True)
     isAdmin = db.BooleanField(default=False)
-    isCliente = db.BooleanField(default=False)
+    isClient = db.BooleanField(default=False)
     isMaster = db.BooleanField(default=False)
-    isConsultor = db.BooleanField(default=False)
-    dataCriacao = db.DateTimeField(default=datetime.now())
-    ultima_atividade = db.DateTimeField(default=datetime.now())
-    familia_id = db.IntegerField(default=0)
+    isManager = db.BooleanField(default=False)
+    begin_datetime = db.DateTimeField(default=datetime.now())
+    last_activity = db.DateTimeField(default=datetime.now())
+    family_id = db.IntegerField(default=0)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
